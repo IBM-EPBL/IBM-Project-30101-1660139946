bc8b6c932c6658ab5a39392738bd4b50aaca4481
Vinothini L <99079881+Vinothini4074-L@users.noreply.github.com>
2022-10-02 13:08:27 +0530

    Add files via upload

---
+++ ASSESMENT/VINOTHINI TL/ASSIGNMENT-1/flask program with five packages/spider.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+from flask import Flask 
+app = flask(_name_)
+@app.route('/')
+def hello_world():
+    return"helloworld"
+if _name_=='_main_'
